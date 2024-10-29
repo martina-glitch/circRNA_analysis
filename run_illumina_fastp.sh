83b14e5b0c4e382a7c5b7493cb815ce94e87f1e6
martina-glitch <126499566+martina-glitch@users.noreply.github.com>
2024-10-29 09:43:31 +0100

    Update run_illumina_fastp.sh

--- run_illumina_fastp.sh
+++ run_illumina_fastp.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
-# filtra i campioni illumina A2 A3 e A4
+# Script for filtering Illumina samples using fastp
+# This script processes Illumina samples A2, A3, and A4, performing adapter trimming,
+# error correction, and quality filtering using fastp. The output includes both filtered
+# sequence files and an HTML quality report for each sample.
 
-# Definisci la lista dei campioni da modificare
-campioni=("A2" "A3" "A4")
+# List of samples to process
+samples=("A2" "A3" "A4")
 
-# Percorso dei file di input e output
-percorso_input="/data/mariachiara/illumina_circRNA/X204SC23075032-Z01-F001_02/01.RawData"
-percorso_output="/data/martina.zambon/results_illumina_fastp"
+# Define input and output directories
+input_path="/data/mariachiara/illumina_circRNA/X204SC23075032-Z01-F001_02/01.RawData"
+output_path="/data/martina.zambon/results_illumina_fastp"
 
-# Loop attraverso i campioni
-for indice in "${!campioni[@]}"; do
-    campione="${campioni[$indice]}"
-    # Costruisci i nomi dei file di input e output
-    input_forward="${percorso_input}/${campione}/${campione}_FKRN24001880$((indice))-1A_H7C75DSXC_L4_1.fq.gz"
-    input_reverse="${percorso_input}/${campione}/${campione}_FKRN24001880$((indice))-1A_H7C75DSXC_L4_2.fq.gz"
-    output_prefix="${percorso_output}/out_${campione}"
-    html_output="${percorso_output}/uscita_fastp_${campione}.html"
+# Iterate over each sample in the list
+for index in "${!samples[@]}"; do
+    sample="${samples[$index]}"
+
+    # Construct input file paths for paired-end reads
+    input_forward="${input_path}/${sample}/${sample}_FKRN24001880$((index))-1A_H7C75DSXC_L4_1.fq.gz"
+    input_reverse="${input_path}/${sample}/${sample}_FKRN24001880$((index))-1A_H7C75DSXC_L4_2.fq.gz"
+
+    # Define output file prefix and HTML report path
+    output_prefix="${output_path}/out_${sample}"
+    html_output="${output_path}/fastp_output_${sample}.html"
     
-
-    # Costruisci il comando fastp
-    comando_fastp=(
+    # Build the fastp command
+    fastp_command=(
         "fastp"
-        "-i" "$input_forward"
-        "-o" "${output_prefix}_1"
-        "-I" "$input_reverse"
-        "-O" "${output_prefix}_2"
-        "${output_prefix}_unpaired1"
-        "${output_prefix}_unpaired2"
-        "${output_prefix}_failed_out"
-        "${output_prefix}_overlapped_out"
-        "-c" "--correction"
-        "--overrepresentation_analysis"
-        "--adapter_sequence" "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"
-        "--adapter_sequence_r2" "GATCGGAAGAGCACACGTCTGAACTCCAGTCACGGATGACTATCTCGTATGCCGTCTTCTGCTTG"
-        "-D" "--dedup" "--dup_calc_accuracy" "6"
-        "--n_base_limit" "3"
-        "-h" "$html_output" "--report_title" "fast_report_${campione}"
-        "--thread" "8"
-        "--trim_poly_g"
-        "--trim_poly_x"
-        "--low_complexity_filter"
-        
+        "-i" "$input_forward"                   # Input file for read 1
+        "-o" "${output_prefix}_1"               # Output file for read 1 after filtering
+        "-I" "$input_reverse"                   # Input file for read 2
+        "-O" "${output_prefix}_2"               # Output file for read 2 after filtering
+        "${output_prefix}_unpaired1"            # Output file for unpaired reads from read 1
+        "${output_prefix}_unpaired2"            # Output file for unpaired reads from read 2
+        "${output_prefix}_failed_out"           # Output file for reads that fail quality filtering
+        "${output_prefix}_overlapped_out"       # Output file for overlapped reads
+        "-c" "--correction"                     # Enable error correction
+        "--overrepresentation_analysis"         # Analyze overrepresented sequences
+        "--adapter_sequence" "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"    # Adapter for read 1
+        "--adapter_sequence_r2" "GATCGGAAGAGCACACGTCTGAACTCCAGTCACGGATGACTATCTCGTATGCCGTCTTCTGCTTG" # Adapter for read 2
+        "-D" "--dedup"                          # Perform duplicate removal
+        "--dup_calc_accuracy" "6"               # Set duplicate calculation accuracy level
+        "--n_base_limit" "3"                    # Limit the number of ambiguous N bases
+        "-h" "$html_output"                     # Generate an HTML report
+        "--report_title" "fast_report_${sample}" # Title for the report
+        "--thread" "8"                          # Number of threads to use
+        "--trim_poly_g"                         # Trim poly-G tails
+        "--trim_poly_x"                         # Trim poly-X tails
+        "--low_complexity_filter"               # Filter out low-complexity sequences
     )
 
-    # Esegui il comando Fastp
-    "${comando_fastp[@]}"
+    # Execute the fastp command
+    "${fastp_command[@]}"
 done
-
